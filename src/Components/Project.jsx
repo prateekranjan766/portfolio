9642650664859e742295c552f8c85d8e58e4df81
Prateek Ranjan <prateekranjan766@gmail.com>
2021-07-09 16:07:38 +0530

    Project Section created

---
+++ src/Components/Project.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import '../styles/ProjectSection.scss';
+
+const Project = ({ project }) => {
+  const [showImage, setShowImage] = useState(true);
+  return (
+    <div
+      className='projects__container--item'
+      onMouseOver={() => setShowImage(false)}
+      onMouseLeave={() => setShowImage(true)}
+    >
+      {showImage == true ? (
+        <img src={project.image} className='project-image' />
+      ) : (
+        <>
+          <div className='project-info'>
+            <p className='project-info__name'>{project.name}</p>
+            <p className='project-info__stack'>{project.techStack}</p>
+            <p className='project-info__description'>{project.description}</p>
+            <div>
+              <a href={project.url} target='_blank' className='project-btn'>
+                Website
+              </a>
+              <a
+                href={project.repository}
+                target='_blank'
+                className='project-btn'
+              >
+                Repository
+              </a>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Project;
--- src/Sections/ProjectSection.jsx
+++ src/Sections/ProjectSection.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import Project from '../Components/Project';
 import '../styles/ProjectSection.scss';
 const ProjectSection = () => {
+  const projects = [
+    {
+      name: 'BookIt',
+      image: '/images/bookIt.png',
+      techStack: 'MongoDB / Express / Node / React',
+      url: 'https://appbookit.herokuapp.com/',
+      repository: 'https://github.com/prateekranjan766/BookIt',
+      description:
+        'An eCommerce platform built with React, Redux, Node, Express and MongoDB for selling books.',
+    },
+    {
+      name: 'Blog-App',
+      image: '/images/blog.png',
+      techStack: 'MongoDB / Express / Node / React',
+      url: 'https://blog-web-ap.herokuapp.com/login',
+      repository: 'https://github.com/prateekranjan766/Blog-App',
+      description:
+        'A full stack CRUD application built with React, Node, Express and MongoDB.',
+    },
+    {
+      name: 'CryptoCurrency-Tracker',
+      image: '/images/crypto.png',
+      techStack: 'React',
+      url: 'https://cryptocurrency-track.web.app/',
+      repository: 'https://github.com/prateekranjan766/cryptocurrency-tracker',
+      description:
+        'A web application built with React which provides a fundamental analysis of the crypto market. It shows various details about all the crypto-currencies with the help of a third party API.',
+    },
+    {
+      name: 'Music Player',
+      image: '/images/music-player.png',
+      techStack: 'React',
+      url: 'https://music-player-3d447.web.app/',
+      repository: 'https://github.com/prateekranjan766/MusicPlayer',
+      description: 'A simple music player application created using react.',
+    },
+  ];
   return (
     <div className='project-section'>
       <h1 className='section__heading'>Projects</h1>
+      <div className='container'>
+        <div className='projects__container'>
+          {projects.map((project) => (
+            <Project project={project} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
