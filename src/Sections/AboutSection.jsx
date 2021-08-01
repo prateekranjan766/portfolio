0b389d1749406fca8daa57dc7c1ac35602eae20d
Prateek Ranjan <prateekranjan766@gmail.com>
2021-08-01 13:25:39 +0530

    Page title and favicon updated, coding profiles section created.

--- src/Sections/AboutSection.jsx
+++ src/Sections/AboutSection.jsx
@@ -16,7 +16,7 @@ const AboutSection = () => {
     'NodeJS',
     'MongoDB',
   ];
-  const values = [80, 70, 80, 55, 80, 70, 60, 60];
+  const values = [80, 70, 80, 60, 80, 65, 60, 60];
   const idx = [0, 1, 2, 3, 4, 5, 6, 7];
 
   return (
@@ -50,6 +50,47 @@ const AboutSection = () => {
               ))}
             </div>
           </Fade>
+        </div>
+        <div className='coding'>
+          <p>Coding Profiles :</p>
+          <div className='coding__profile'>
+            <a
+              href='https://leetcode.com/PrateekRanjan/'
+              className='coding__profile--item'
+            >
+              Leetcode
+            </a>
+            <a
+              href='https://auth.geeksforgeeks.org/user/prateekranjan766/practice/'
+              className='coding__profile--item'
+            >
+              GFG
+            </a>
+            <a
+              href='https://www.hackerrank.com/prateekranjan766'
+              className='coding__profile--item'
+            >
+              HackerRank
+            </a>
+            <a
+              href='https://www.codechef.com/users/prateek_ranjan'
+              className='coding__profile--item'
+            >
+              Codechef
+            </a>
+            <a
+              href='https://codeforces.com/profile/prateek_ranjan'
+              className='coding__profile--item'
+            >
+              Codeforces
+            </a>
+            <a
+              href='https://www.hackerearth.com/@prateek973'
+              className='coding__profile--item'
+            >
+              HackerEarth
+            </a>
+          </div>
         </div>
       </section>
     </>
