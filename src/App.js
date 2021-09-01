8c57afff4618cbd34eff78dfd4ef446cd9c498c5
Prateek Ranjan <prateekranjan766@gmail.com>
2021-09-01 22:36:35 +0530

    Loader Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import LandingSection from './Sections/LandingSection';
-import AboutSection from './Sections/AboutSection';
-import ProjectSection from './Sections/ProjectSection';
-import ContactSection from './Sections/ContactSection';
+import React, { Suspense, lazy } from 'react';
+import Loader from './Components/Loader.jsx';
+const LandingSection = lazy(() => import('./Sections/LandingSection'));
+const AboutSection = lazy(() => import('./Sections/AboutSection'));
+const ProjectSection = lazy(() => import('./Sections/ProjectSection'));
+const ContactSection = lazy(() => import('./Sections/ContactSection'));
 const App = () => {
   return (
     <main>
-      <LandingSection />
-      <AboutSection />
-      <ProjectSection />
-      <ContactSection />
+      <Suspense fallback={<Loader />}>
+        <LandingSection />
+        <AboutSection />
+        <ProjectSection />
+        <ContactSection />
+      </Suspense>
     </main>
   );
 };
---
+++ src/Components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <i className='fas fa-circle-notch spinner'></i>
+    </div>
+  );
+};
+
+export default Loader;
